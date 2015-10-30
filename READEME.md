7747589105521740cd21bd018078e8e8629d93b1
2betop <liaoxuezhi@icloud.com>
2015-10-30 11:38:19 +0800

    update

---
+++ READEME.md
@@ -0,0 +1,20 @@
+# FIS3 Angular2 App 脚手架
+
+## 安装 fis3
+
+`npm install fis3 -g`
+
+## 初始化
+
+```bash
+mkdir demo
+cd demo
+fis3 init angular2-app
+```
+
+## 运行 & 预览
+
+```bash
+fis3 release
+fis3 server start
+```
---
+++ fis-conf.js
@@ -0,0 +1,17 @@
+fis.hook('system');
+
+fis.match('*.ts', {
+  parser: fis.plugin('typescript'),
+  rExt: '.js',
+  isMod: true
+});
+
+fis.match('/angular2.dev.js', {
+  isMod: false
+});
+
+fis.match('::package', {
+  postpackager: fis.plugin('loader', {
+    useInlineMap: true
+  })
+});
